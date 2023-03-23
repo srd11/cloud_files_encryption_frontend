ef8bb5d02dcdb6f16ff96d6fcc09a0b63e92763f
thisisommore <ommore501@gmail.com>
2023-03-23 07:32:00 +0000

    Handle undefined key

--- src/encryption/tools.ts
+++ src/encryption/tools.ts
@@ -5,7 +5,7 @@ export const LOCAL_KEY = "ENCRYTION_KEY"
 
 export const encrypt = (a: string) => {
     const key = localStorage.getItem(LOCAL_KEY) as string
-    let encrypted_text = AES.encrypt(JSON.stringify({ a }), key).toString();
+    let encrypted_text = AES.encrypt(JSON.stringify({ a }), key ?? "").toString();
     encrypted_text = Buffer.from(encrypted_text).toString("hex")
 
     return encrypted_text
@@ -16,7 +16,7 @@ export const decrypt = (a: string) => {
 
     const key = localStorage.getItem(LOCAL_KEY) as string
     a = Buffer.from(a, "hex").toString("utf8")
-    const decrpyted = AES.decrypt(a, key).toString(enc.Utf8)
-
+    const decrpyted = AES.decrypt(a, key ?? "").toString(enc.Utf8)
+    if (!decrpyted) return ""
     return JSON.parse(decrpyted).a
 } 
